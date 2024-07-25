224663c6587fabceae21d7a4edcb5bec7f9ffa16
VoThanhAn28 <85780063+VoThanhAn28@users.noreply.github.com>
2024-07-25 09:04:55 +0700

    update

--- Lottery/js/FAQ.js
+++ Lottery/js/FAQ.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const widgetItems = document.querySelectorAll('.main-content .content .widget ul li .widget-item ');
+    const widgetItems = document.querySelectorAll('.main-content .content .widget ul li .widget-item h6');
 
     widgetItems.forEach(function (item) {
         item.addEventListener('click', function () {
@@ -7,14 +7,18 @@ document.addEventListener('DOMContentLoaded', function () {
             const icon = this.nextElementSibling;
             const isActive = parentLi.classList.contains('active');
 
-            // Đóng tất cả các mục khác
-            document.querySelectorAll('.main-content .content .widget ul li').forEach(function (li) {
-                li.classList.remove('active');
-                li.querySelector('.widget-item i').classList.remove('minus');
-                li.querySelector('.widget-item i').classList.add('plus');
-            });
+            if (isActive) {
+                parentLi.classList.remove('active');
+                icon.classList.remove('minus');
+                icon.classList.add('plus');
+            } else {
+                // Đóng tất cả các mục khác
+                document.querySelectorAll('.main-content .content .widget ul li').forEach(function (li) {
+                    li.classList.remove('active');
+                    li.querySelector('.widget-item i').classList.remove('minus');
+                    li.querySelector('.widget-item i').classList.add('plus');
+                });
 
-            if (!isActive) {
                 parentLi.classList.add('active');
                 icon.classList.remove('plus');
                 icon.classList.add('minus');
